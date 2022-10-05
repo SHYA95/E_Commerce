113ceedd5d50898a811e135404d1180129e8053c
SHYA95 <yassershrouk254@gmail.com>
2022-10-05 15:52:50 +0200

    login and signup page

--- E_Commerce/ViewController.swift
+++ E_Commerce/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var PasswordTextField: UITextField!
+    @IBOutlet weak var EmailTextField: UITextField!
+    @IBOutlet weak var EmailView: UIView!
+    @IBOutlet weak var PasswordView: UIView!
+    @IBOutlet weak var loginButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+      
+        addLayer(View: EmailView)
+        addLayer(View: PasswordView)
+        addLayer(View: loginButton)
+        
+        
     }
 
-
+    func addLayer (View:UIView){
+       View.layer.cornerRadius=10
+       View.layer.shadowColor=UIColor.lightGray.cgColor
+       View.layer.shadowOffset=CGSize(width: 2, height: 3)
+        
+let shadowPath = UIBezierPath(roundedRect:View.bounds, cornerRadius: 10)
+        View.layer.shadowPath=shadowPath.cgPath
+        View.layer.shadowOpacity=0.5
+        
+    }
 }
 
