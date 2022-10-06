5c8eaf3d949c104c4f25c899d03ec829344b46e2
SHYA95 <yassershrouk254@gmail.com>
2022-10-06 23:44:45 +0200

    Forget Screen Page added !

---
+++ E_Commerce/ForgetPassViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ForgetPassViewController.swift
+//  E_Commerce
+//
+//  Created by Shrouk Yasser on 06/10/2022.
+//
+
+import Foundation
+import UIKit
+
+class ForgetPassViewController: UIViewController {
+    @IBOutlet weak var EmailTextField: UITextField!
+    @IBOutlet weak var EmailView: UIView!
+    @IBOutlet weak var SendButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        SendButton.addLayer()
+        EmailView.addLayer()
+    
+    }}
+
